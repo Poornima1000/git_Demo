8d3c459409feeef39563c429cb6e30bc7967d21a
Purnima1000 <kurli.purnima@infokalash.com>
2023-09-22 18:35:13 +0530

    demoProj

---
+++ DemoPractice/src/test/java/com/infokalash/Hrms/CTC/DemoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infokalash.Hrms.CTC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
